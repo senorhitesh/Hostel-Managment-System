173958015c3f3346d94557cac98b5c7dbdce6da3
senorhitesh <senorhitesh@gmail.com>
2026-02-14 23:24:21 +0530

    edge cases for form

--- EMS/src/components/Auth/Login.jsx
+++ EMS/src/components/Auth/Login.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Login/Button";
 import InputLable from "./Login/InputLable";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [sumbitted, setSumbitted] = useState(false)
+
+  const SubmitHandler = (e) => {
+    e.preventDefault();
+      if (!email || !password) {
+    setSumbitted(true);
+    return;
+  }
+    setSumbitted(false)
+    setEmail("");
+    setPassword("");
+  };
   return (
-    <div className="relative min-h-screen flex items-center justify-center bg-white overflow-hidden flex-col max-w-5xl mx-auto md:mx-2 sm:mx-4 sm:w-full">
+    <div
+      className="  relative
+              w-full
+              min-h-screen
+              flex flex-col
+              items-center
+              justify-center
+              bg-white
+              overflow-hidden
+              px-4 sm:px-6 md:px-8   
+              lg:max-w-5xl           
+              lg:mx-auto
+              
+              "
+    >
       <div
         className="
              absolute left-0 top-0 h-full w-12
@@ -21,7 +49,12 @@ const Login = () => {
            bg-[repeating-linear-gradient(315deg,#e5e7eb_0,#e5e7eb_1px,transparent_0,transparent_50%)]
            bg-[size:10px_10px]"
       />
-      <form className="flex flex-col h-fit justify-center gap-2 bg-white p-6 w-1/2 rounded-md shadow-[0px_4px_32px_0_rgba(99,102,241,.10)] hover:shadow-[0px_4px_32px_0_rgba(99,102,241,.15)] transition-all ">
+      <form
+        onSubmit={(val) => {
+          SubmitHandler(val);
+        }}
+        className="flex flex-col h-fit justify-center gap-2 bg-white p-6 w-1/2 rounded-md shadow-[0px_4px_32px_0_rgba(99,102,241,.10)] hover:shadow-[0px_4px_32px_0_rgba(99,102,241,.15)] transition-all "
+      >
         <h1
           className="
                inline-block
@@ -37,25 +70,33 @@ const Login = () => {
           Suthar Samaj Hostel
         </h1>
         <InputLable
+          setValue={setEmail}
+          value={email}
           name="email"
           type="email"
           placeholder="Enter your Email"
           label="Email"
+          submitted={sumbitted}
         />
         <InputLable
+          setValue={setPassword}
+          value={password}
           name="Password"
           type="password"
           placeholder="*********"
           label="Password"
+          submitted={sumbitted}
         />
         <Button />
+        <p className="self-center items-center flex text-gray-400">Don't have an account? <span className="text-bg-500 cursor-pointer hover:text-blue-600 font-medium">Sign Up</span></p>
       </form>
-      <p
+      {/* <p
         className="text-gray-400 cursor-pointer  
       py-1 px-4 rounded-full hover:text-gray-700 transtion hover:bg-gray-100 "
       >
         Agar Id nah hoto office me sampark kare
-      </p>
+      </p> */}
+
     </div>
   );
 };
--- EMS/src/components/Auth/Login/InputLable.jsx
+++ EMS/src/components/Auth/Login/InputLable.jsx
@@ -1,36 +1,47 @@
 import React from "react";
 import BgGrid from "./BgGrid";
 
-const InputLable = ({ name, type, label, placeholder }) => {
+const InputLable = ({
+  name,
+  type,
+  label,
+  placeholder,
+  value,
+  setValue,
+  submitted,
+}) => {
   return (
     <>
-      <div className="flex flex-col gap-2 px-2 py-4 rounded-md has-user-invalid:bg-red-50">
+      <div className={`flex flex-col gap-2 px-2 py-4 rounded-md ${submitted && !value ? `bg-red-50 `: ""}`}>
         <label
           htmlFor={name}
-          className="text-lg text-neutral-900 after:content-['*'] after:ml-1 after:text-red-500 font-medium"
+          className= "text-lg text-neutral-900 after:content-['*'] after:ml-1 after:text-red-500 font-medium"
         >
           {label}
         </label>
 
         <input
-          required
+          value={value}
+          onChange={(val) => {
+            setValue(val.target.value);
+          }}
           id={name}
           name={name}
           type={type}
           placeholder={placeholder}
           autoComplete="off"
-          className="
-          peer
-          px-4 py-2 font-medium bg-gray-50
-          border rounded-md border-gray-400
-          focus:outline-none focus:ring-2 focus:ring-neutral-300
-          transition-all duration-200
+          className={`
+  px-4 py-2 font-medium bg-gray-50
+  border rounded-md
+  focus:outline-none focus:ring-2 focus:ring-neutral-300
+  transition-all duration-200
 
-          user-invalid:border-red-400
-          user-invalid:ring-2
-          user-invalid:ring-red-400/60
-          user-invalid:bg-red-100
-        "
+  ${
+    submitted && !value
+      ? "border-red-400 ring-2 ring-red-400/60 bg-red-50"
+      : "border-gray-400"
+  }
+`}
         />
       </div>
 
