3a982d0119f850805dd516c9452892dcdcf3a751
senorhitesh <senorhitesh@gmail.com>
2026-02-16 23:09:14 +0530

    ui polished -Student DashBoard

--- src/components/DashBoard/EmployeeDashboad.jsx
+++ src/components/DashBoard/EmployeeDashboad.jsx
@@ -3,19 +3,19 @@ import HeaderED from "./components/Header-ED";
 import TaskCard from "./components/TaskCard";
 const EmployeeDashboad = () => {
   return (
-    <div className="bg-red-100 h-screen max-w-6xl mx-auto py-2">
+    <div className="h-screen max-w-6xl mx-auto py-2">
       <HeaderED />
-      <div className="bg-red-200 mt-8">
+      <div className="mt-8">
         <TaskCard/>
       </div>
       <div className=" m-4">
-        <div className="w-full bg-red-300 p-4 rounded-xl">
+        <div className="w-full bg-[#FFFFF0]  shadow-lg border-1 border-transparent transtion-all duration-150 hover:shadow-2xl hover:scale-101 hover:bg-gray-100 hover:border-gray-300  p-4 rounded-xl active:scale-99">
           <div className="flex justify-between mb-3 ">
             <p className="bg-green-800 text-white px-3  rounded-xl">New</p>
             <p>16-02-2026</p>
           </div>
           <div>
-            <p>Title</p>
+            <p className="text-2xl text-gray-900 font-bold"> Title</p>
             <p>Description</p>
           </div>
         </div>
--- src/components/DashBoard/components/Header-ED.jsx
+++ src/components/DashBoard/components/Header-ED.jsx
@@ -4,12 +4,12 @@ import { LogOut } from "lucide-react";
 const HeaderED = () => {
   return (
     <>
-          <div className="bg-gray-100 px-8 mt-4 justify-between flex items-center  ">
+          <div className=" px-8 mt-4 justify-between flex items-center  ">
           <div className="flex flex-col py-1">
           <h1 className="text-sm font-medium">Welcome back</h1>
           <p className="text-3xl font-bold ">Hitesh <span>🧠</span></p>
         </div>
-        <button className="cursor-pointer flex gap-2 bg-red-600 text-white py-2 font-medium rounded-md hover:bg-red-600/90 px-4 transtion-all shadow-md hover: duration-150 active:scale-98 hover:-translate-y-0.5 shadow-[0px_4px_32px_0_rgba(99,102,241,.70)] group group/arrow">
+        <button className="cursor-pointer flex gap-2 text-red-500 border-2 hover:text-white border-red-300 py-2 font-medium rounded-md hover:bg-red-600/90 px-4 transtion-all hover:border-transparent hover:shadow-md  hover: duration-150 active:scale-98 hover:-translate-y-0.5 ">
           Log Out <span><LogOut className="" /></span>
         </button>
       </div>
