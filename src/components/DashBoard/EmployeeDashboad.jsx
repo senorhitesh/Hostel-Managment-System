b92a712cc51f365f92daa6937aa051d1a2e0eb36
senorhitesh <senorhitesh@gmail.com>
2026-02-16 23:48:51 +0530

    change

--- src/components/DashBoard/EmployeeDashboad.jsx
+++ src/components/DashBoard/EmployeeDashboad.jsx
@@ -9,7 +9,7 @@ const EmployeeDashboad = () => {
         <TaskCard/>
       </div>
       <div className=" m-4">
-        <div className="w-full bg-[#FFFFF0]  shadow-lg border-1 border-transparent transtion-all duration-150 hover:shadow-2xl hover:scale-101 hover:bg-gray-100 hover:border-gray-300  p-4 rounded-xl active:scale-99">
+        <div className="w-full bg-[#FFFFF0]  shadow-lg border border-transparent transtion-all duration-150 hover:shadow-2xl hover:scale-101 hover:bg-gray-100 hover:border-gray-300  p-4 rounded-xl active:scale-99">
           <div className="flex justify-between mb-3 ">
             <p className="bg-green-800 text-white px-3  rounded-xl">New</p>
             <p>16-02-2026</p>
